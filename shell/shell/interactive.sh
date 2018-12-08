12ddf765a3a54bd6250ea71c8f551e96a8d5ae75
Dimitar Pavlov <dimpavloff@gmail.com>
2018-12-08 23:49:54 +0000

    add fn to update go tools

--- shell/shell/interactive.sh
+++ shell/shell/interactive.sh
@@ -27,3 +27,19 @@ function glf() { git log --all --grep="$1"; }
 
 # run emacs daemon if not running and connect
 alias emacs='emacsclient -c -a ""'
+
+updateGoToolchain() {
+    go get -u -v github.com/mdempsky/gocode
+    go get -u -v github.com/rogpeppe/godef
+    go get -u -v golang.org/x/tools/cmd/guru
+    go get -u -v golang.org/x/tools/cmd/gorename
+    go get -u -v golang.org/x/tools/cmd/goimports
+    go get -u -v github.com/zmb3/gogetdoc
+    go get -u -v github.com/cweill/gotests/...
+    go get -u github.com/haya14busa/gopkgs/cmd/gopkgs
+    go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+    go get -u github.com/josharian/impl
+    go get -u -v github.com/godoctor/godoctor
+    go install github.com/godoctor/godoctor
+    go get -u -v github.com/fatih/gomodifytags
+}
